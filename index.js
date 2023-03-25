b93cae0e9c89a6bd88eae7e07f3c63c8b817c243
Gauravpadam <1032201077@tcetmumbai.in>
2023-03-25 05:32:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// An express server, Which will handle stuff
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/', (req, res) => {
+    res.json({
+        message: "Hello World!"
+    });
+});
+
+app.listen(port, () => {
+    console.log('Example app listening');
+});
